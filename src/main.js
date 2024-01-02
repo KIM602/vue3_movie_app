d082a478a6c8101afe92cb07085bb9fc0cefa770
KIM602 <kyk940608@gmail.com>
2024-01-02 11:24:40 +0900

    Vuex(Store)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './routes/index.js'
+import router from './routes' //index.js 파일 생략 가능
+import store from './store'
 
 createApp(App)
     .use(router)
+    .use(store)
     .mount('#app')
